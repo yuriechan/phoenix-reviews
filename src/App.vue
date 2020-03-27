463ca96a9c34af2deab3f6e8a05bc2d5ca2d868b
Yurie <cutecupcakeandcute@gmail.com>
2020-03-27 23:36:43 +0900

    add carousels component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <v-app>
     <v-content>
        <SearchBar/>
+       <Carousels/>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import SearchBar from '@/components/SearchBar.vue'
+import Carousels from '@/components/Carousels.vue'
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    Carousels
   },
 
   data: () => ({}),
