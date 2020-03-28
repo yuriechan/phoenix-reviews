33604e107747c19533faa813d15070b427437aa1
Yurie <cutecupcakeandcute@gmail.com>
2020-03-28 15:08:39 +0900

    add login btn

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,7 +3,12 @@
         <v-toolbar color="purple" dark flat>
             <v-text-field class="mx-4" flat hide-details label="Search" :prepend-inner-icon="svgPath" solo-inverted
             ></v-text-field>
+            <v-spacer></v-spacer>
+            <v-btn text outlined>
+                <span class="mr-2">Log in</span>
+            </v-btn>
         </v-toolbar>
+        
     </v-card>
 </template>
 
