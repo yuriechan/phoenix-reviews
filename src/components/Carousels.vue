a725146e73fa3648bf01c095412160e6de003aaf
Yurie <cutecupcakeandcute@gmail.com>
2020-03-28 18:15:20 +0900

    set only one placeholder for carousel's color and slide

--- src/components/Carousels.vue
+++ src/components/Carousels.vue
@@ -1,9 +1,11 @@
 <template>
   <v-carousel cycle height="400" hide-delimiter-background show-arrows-on-hover>
-    <v-carousel-item v-for="(slide, i) in slides" :key="i">
-      <v-sheet :color="colors[i]" height="100%">
+    <v-carousel-item>
+      <v-sheet :color="color" height="100%">
         <v-row class="fill-height" align="center" justify="center">
-        <div class="display-3">{{ slide }} Slide</div>
+          <p class="display-3">
+            {{ slide }} Slide
+          </p>
         </v-row>
       </v-sheet>
     </v-carousel-item>
@@ -14,20 +16,8 @@
 export default {
     data() {
         return {
-            colors: [
-                'indigo',
-                'warning',
-                'pink darken-2',
-                'red lighten-1',
-                'deep-purple accent-4',
-            ],
-            slides: [
-                'First',
-                'Second',
-                'Third',
-                'Fourth',
-                'Fifth',
-            ],
+            color: 'deep-purple accent-4',
+            slide: 'First',
         }
     }
 }
