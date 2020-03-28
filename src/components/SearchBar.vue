8f0116461c71c026fd243e528bc9a9ea9f41b046
Yurie <cutecupcakeandcute@gmail.com>
2020-03-28 18:18:05 +0900

    rename name of data and format code

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,8 +1,7 @@
 <template>
     <v-card>
         <v-toolbar color="purple" dark flat>
-            <v-text-field class="mx-4" flat hide-details label="Search" :prepend-inner-icon="svgPath" solo-inverted
-            ></v-text-field>
+            <v-text-field class="mx-4" flat hide-details label="Search" :prepend-inner-icon="icon" solo-inverted></v-text-field>
         </v-toolbar>
     </v-card>
 </template>
@@ -11,7 +10,7 @@
 import { mdiMagnify } from '@mdi/js'  
 export default {
     data: () => ({
-        svgPath: mdiMagnify
+        icon: mdiMagnify
     })
 }
 </script>
