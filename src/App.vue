967cb176cd868e8892af2ae86ec55ea57354ca5b
Yurie <cutecupcakeandcute@gmail.com>
2020-03-31 01:05:24 +0900

    move search bar to navigation

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <v-app>
-      <Navigation/>
     <v-content>
-       <SearchBar/>
+      <Navigation/>
        <Carousels/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import SearchBar from '@/components/SearchBar.vue'
 import Carousels from '@/components/Carousels.vue'
 import Navigation from '@/components/Navigation.vue'
 export default {
   name: 'App',
   components: {
-    SearchBar,
     Carousels,
     Navigation
   },
