73e6e01fb7e2a31debfd854120f811ea775191cf
Yurie <cutecupcakeandcute@gmail.com>
2020-03-31 21:56:40 +0900

    add input field for sign up

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -51,20 +51,20 @@
                         <v-card-text>
                             <v-container>
                                 <v-row>
-                                    <v-col>
-                                        <v-text-field></v-text-field>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field label="First Name*"></v-text-field>
                                     </v-col>
-                                     <v-col>
-                                        <v-text-field></v-text-field>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field label="Last Name*"></v-text-field>
                                     </v-col>
-                                     <v-col>
-                                        <v-text-field></v-text-field>
+                                    <v-col cols="12" sm="6">
+                                        <v-text-field label="Username*"></v-text-field>
                                     </v-col>
-                                     <v-col>
-                                        <v-text-field></v-text-field>
+                                    <v-col cols="12">
+                                        <v-text-field label="Email*" required></v-text-field>
                                     </v-col>
-                                     <v-col>
-                                        <v-text-field></v-text-field>
+                                    <v-col cols="12">
+                                        <v-text-field label="Password*" type="password" required></v-text-field>
                                     </v-col>
                                 </v-row>
                             </v-container>
@@ -74,10 +74,10 @@
                         </v-card-text>
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            <v-btn>
+                            <v-btn color="blue darken-1" text @click="dialog = false">
                                 Already have an account?
                             </v-btn>
-                            <v-btn>
+                            <v-btn color="blue darken-1" text @click="dialog = false">
                                 Sign up
                             </v-btn>
                         </v-card-actions>
